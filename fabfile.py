644004019dd093def026c259192217812a1e8030
Léonard TAVAE <leonard.tavae@gmail.com>
2014-04-20 00:58:48 -1000

    Factorisation de la commande de dump.

--- fabfile.py
+++ fabfile.py
@@ -6,11 +6,15 @@ def filebackup():
     sudo('cd /var/backups/fabric/files && tar -cvzf www.tna.pf.tar.gz /var/www/www.tna.pf')
     sudo('cd /var/backups/fabric/files && tar -cvzf doc.tna.pf.tar.gz /var/www/doc.tna.pf')
 
-def dbdump(password):
-    sudo ('cd /var/backups/fabric/db && mysqldump -u root -p%s wwwtnapf > wwwtnapf.sql' %password)
-    sudo ('cd /var/backups/fabric/db && mysqldump -u root -p%s mediawiki > mediawiki.sql' %password)
-    sudo ('cd /var/backups/fabric/db && mysqldump -u root -p%s mumble > mumble.sql' %password)
-    sudo ('cd /var/backups/fabric/db && mysqldump -u root -p%s archivetnapf > archivetnapf.sql' %password)
+def mysqldump(user, password, database):
+    sudo('mysqldump -u %s -p%s %s > %s.sql' %(user, password, database, database))
+
+def dbdump(user,password):
+    with cd('/var/backups/fabric/db'):
+        mysqldump(user, password, 'wwwtnapf')
+        mysqldump(user, password, 'mediawiki')
+        mysqldump(user, password, 'mumble')
+        mysqldump(user, password, 'archivetnapf')
 
 def slapdump():
     sudo ('cd /var/backups/fabric/db && slapcat > annuaire.ldif')
