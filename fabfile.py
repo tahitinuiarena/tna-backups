f9db7f38430df9a22b5557d34642c82db20ecf80
Léonard TAVAE <leonard.tavae@gmail.com>
2014-04-20 17:03:46 -1000

    Prises en compte de la totalité des données du fichier de configuration.

--- fabfile.py
+++ fabfile.py
@@ -2,35 +2,59 @@ from fabric.api import *
 from os import path
 import yaml
 
-stream = open("tna.yaml", 'r')
+stream = open("conf.yaml", 'r')
 conf = yaml.load(stream)
 
-env.hosts = ['srv1.tna.pf']
+def getconfig(server, what, password):
+    if what == 'all':
+        # Tout les whats
+        if 'ldap' in conf[server]:
+            print 'LDAP'
+            with cd('/var/backups/fabric/db'):
+                for database in conf[server]['ldap']:
+                    ldapdump(conf[server]['config']['ldapuser'], password, database)
+        if 'mysql' in conf[server]:
+            print 'MYSQL'
+            with cd('/var/backups/fabric/db'):
+                for database in conf[server]['mysql']:
+                    mysqldump(conf[server]['config']['mysqluser'], password, database)
+        if 'files' in conf[server]:
+            print 'FILE'
+            with cd('/var/backups/fabric/files'):
 
-def filecopy(path, archive_name):
-    with cd('/var/backups/fabric/files'):
-        sudo('tar -cvzf %s.tar.gz %s' %(archive_name, path))
+                for archives in conf[server]['files']:
+                    directory = ''
+                    for directories in conf[server]['files'][archives]:
+                        directory = directory + ' '+ directories
+                    filedump(archives, directory)
+    else:
+        if what in conf[server]:
+            if what == 'ldap':
+                with cd('/var/backups/fabric/db'):
+                    for database in conf[server]['ldap']:
+                        ldapdump(conf[server]['config']['ldapuser'], password, database)
+            if what == 'mysql':
+                with cd('/var/backups/fabric/db'):
+                    for database in conf[server]['mysql']:
+                        mysqldump(conf[server]['config']['mysqluser'], password, database)
+            if what == 'files':
+                with cd('/var/backups/fabric/files'):
+                    for archives in conf[server]['files']:
+                        directory = ''
+                        for directories in conf[server]['files'][archives]:
+                            directory = directory + ' '+ directories
+                        filedump(archives, directory)
+        else:
+            print("No %s in config file." %what)
 
-def filedump():
-    filecopy('/var/www/www.tna.pf', 'www.tna.pf')
-    filecopy('/var/www/doc.tna.pf', 'doc.tna.pf')
-
-#def config():
-#    print conf['srv1.tna.pf']['config']
+def filedump(files, directory):
+    sudo('tar -cvzf %s.tar.gz %s' %(files, directory))
 
 def mysqldump(user, password, database):
     sudo('mysqldump -u %s -p%s %s > %s.sql' %(user, password, database, database))
 
-def dbdump(user,password):
-    with cd('/var/backups/fabric/db'):
-        mysqldump(user, password, 'wwwtnapf')
-        mysqldump(user, password, 'mediawiki')
-        mysqldump(user, password, 'mumble')
-        mysqldump(user, password, 'archivetnapf')
-
-def slapdump():
-    with cd('/var/backups/fabric/db'):
-        sudo ('slapcat > annuaire.ldif')
+def ldapdump(user, password, database):
+    sudo ('slapcat > annuaire.ldif')
 
 def dbbackup(password):
     dbdump(password)
@@ -39,3 +63,16 @@ def dbbackup(password):
     sudo ('cd /var/backups/fabric/db && rm *.bz2 && bzip2 *')
     get('/var/backups/fabric/db/*', 'databases/')
     get('/var/backups/fabric/files/*', 'files/')
+
+@task
+def backup(hostname = 'all', what = 'all', password = ''):
+    if hostname == 'all':
+        for server in conf:
+            env.host_string = server
+            getconfig(server, what, password)
+    else:
+        for server in conf:
+            if server == hostname:
+                env.host_string = server
+                getconfig(server, what, password)
+
